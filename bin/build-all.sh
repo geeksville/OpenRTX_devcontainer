93bcd02d3f5555bedbf8e624d516912bb52bc2a8
Kevin Hester <kevinh@geeksville.com>
2025-03-02 10:44:50 +0800

    make "build-all.sh" test script work

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -2,8 +2,13 @@ set -e
 
 # a crude script to just build all of the ttpwr binaries
 
-mkdir openrtx-build || true
-cd openrtx-build
-west init -m https://github.com/geeksville/OpenRTX.git --mr dev
+if [ ! -d ".west" ]; then
+  echo "Fetching reference OpenRTX repo..."
+  west init -m https://github.com/geeksville/OpenRTX.git --mr dev
+fi
 west update
 source zephyr/zephyr-env.sh # You need to execute this for every new shell
+
+cd OpenRTX
+rm -rf build || true
+meson setup build; meson compile -C build openrtx_ttwrplus
