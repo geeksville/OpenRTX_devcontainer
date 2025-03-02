8fdd1ff0945a97f12c0ab3a4580e2c33858124bf
Kevin Hester <kevinh@geeksville.com>
2025-03-02 15:58:54 +0800

    add support for building arm radios (mod17?)

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -9,6 +9,22 @@ fi
 west update
 source zephyr/zephyr-env.sh # You need to execute this for every new shell
 
+ARMTARGET=mod17
+echo "Building for the $ARMTARGET"
+cd OpenRTX
+rm -rf build || true
+meson setup --cross-file cross_arm.txt build_arm
+meson compile -C build_arm openrtx_$ARMTARGET
+cd ..
+
+echo "Linux emulator build disabled FIXME"
+# doesn't work yet due to /usr/bin/ld: /usr/lib/i386-linux-gnu/libSDL2.so: error adding symbols: file in wrong format
+# I think because the lib is 386 only and the rest of the build is amd64?
+# meson setup build_linux
+# meson compile -C build_linux openrtx_linux
+
+echo "Building for the TTWRPlus"
 cd OpenRTX
 rm -rf build || true
 meson setup build; meson compile -C build openrtx_ttwrplus
+cd ..
